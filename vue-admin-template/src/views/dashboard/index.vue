f479a57e00f35ba4b8bc03888b90cbae0f51f89e
zk1239520941 <77726841+zk1239520941@users.noreply.github.com>
2021-01-20 03:07:36 -0800

    Add files via upload

---
+++ vue-admin-template/src/views/dashboard/index.vue
@@ -0,0 +1,47 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: zhangkun
+ * @Date: 2021-01-20 18:55:34
+ * @LastEditors: zhangkun
+ * @LastEditTime: 2021-01-20 19:00:44
+-->
+<template>
+  <div id="app">
+    <butterfly-vue
+      :canvasData="mockData"
+      @onCreateEdge="logEvent"
+      @onChangeEdges="logEvent"
+      @onDeleteEdge="logEvent"
+      @onOtherEvent="logEvent"
+      @onLoaded="finishLoaded"
+    />
+  </div>
+</template>
+
+<script>
+import ButterflyVue from 'butterfly-vue';
+import 'butterfly-vue/index.css';
+import mockData from "./mockData";
+
+export default {
+  name: 'App',
+  components: {
+    ButterflyVue
+  },
+  data(){
+    return{
+      mockData
+    }
+  },
+  methods:{
+    logEvent(e) {
+      console.log(e);
+    },
+    finishLoaded(canvans) {
+      console.log(canvans);
+      console.log("finish");
+    },
+  }
+}
+</script>
